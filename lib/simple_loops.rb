b00520b1cb5b54f63f35b2f1bafd5417f15e4b1a
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-08 04:47:12 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,8 +8,16 @@ end
 
 def loop_message_n_times(message, n)
   counter = 0
-  while counter < n
+  while counter < n do
    puts message
    counter += 1
   end
 end
+
+def  output_array(message, n)
+  counter = 0
+  while counter < n do
+   puts message
+   counter += 1
+  end
+end 
