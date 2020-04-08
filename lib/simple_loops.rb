364f1c6e8b4e21d7ab0d21504e2c2102a97dc393
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-08 04:54:03 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -21,3 +21,11 @@ def  output_array(array)
    counter += 1
   end
 end 
+
+def return_string_array(array)
+  counter = 0 
+  while counter < array.length do
+    new_array = array[counter].to_s
+    counter += 1
+  end
+end
